7f063539e525a89d6898b936fec656432e37cdf0
zzZDenisZzz <CoRey2007@bigmir.net>
2018-02-16 21:06:36 +0200

    Configure rest services.

--- src/main/java/com/qoobico/remindme/server/controller/ReminderController.java
+++ src/main/java/com/qoobico/remindme/server/controller/ReminderController.java
@@ -1,19 +1,30 @@
 package com.qoobico.remindme.server.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import com.qoobico.remindme.server.entity.Remind;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Date;
+
+@RestController
 @RequestMapping("/reminder")
 public class ReminderController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getReminder(ModelMap model) {
-        return "My reminder";
+    public Remind getReminder() {
+        return createMockRemind();
+    }
+
+    private Remind createMockRemind() {
+        Remind remind = new Remind();
+        remind.setId(1);
+        remind.setRemindDate(new Date());
+        remind.setTitle("My first remind");
+
+        return remind;
     }
 
 }
